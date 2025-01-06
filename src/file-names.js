9886bb87adb5bcbd2c779606a99c1717e9437c53
tursynkhan <tursynkhan>
2025-01-06 23:49:05 +0500

    feat: solved all tasks

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const nameMap = new Map();
+  const result = [];
+
+  for (const name of names) {
+    if (nameMap.has(name)) {
+      let count = nameMap.get(name);
+      let newName = `${name}(${count})`;
+
+      while (nameMap.has(newName)) {
+        count++;
+        newName = `${name}(${count})`;
+      }
+
+      nameMap.set(name, count + 1);
+      nameMap.set(newName, 1);
+      result.push(newName);
+    } else {
+      nameMap.set(name, 1);
+      result.push(name);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,76 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect; // Определяем тип машины (прямая или обратная)
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    return this.process(message, key, true);
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    return this.process(encryptedMessage, key, false);
+  }
+
+  process(input, key, isEncrypting) {
+    input = input.toUpperCase();
+    key = key.toUpperCase();
+
+    const result = [];
+    const A = 65;
+    const Z = 90;
+    const alphabetLength = 26;
+
+    let keyIndex = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const charCode = input.charCodeAt(i);
+
+      if (charCode >= A && charCode <= Z) {
+        const keyCharCode = key.charCodeAt(keyIndex % key.length) - A;
+        const shift = isEncrypting
+          ? (charCode - A + keyCharCode) % alphabetLength
+          : (charCode - A - keyCharCode + alphabetLength) % alphabetLength;
+
+        result.push(String.fromCharCode(A + shift));
+        keyIndex++;
+      } else {
+        result.push(input[i]);
+      }
+    }
+
+    return this.isDirect ? result.join("") : result.reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
