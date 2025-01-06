acf81770aee10ad445902a53dc30ba2755502717
tursynkhan <tursynkhan>
2025-01-07 01:29:32 +0500

    feat: splve waht-season

--- src/what-season.js
+++ src/what-season.js
@@ -12,7 +12,31 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function getSeason(date) {
-  throw new NotImplementedError("Not implemented");
+  if (arguments.length == 0) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (!(date instanceof Date)) {
+    throw new Error("Invalid date!");
+  }
+
+  if (
+    Object.getOwnPropertyNames(date).sort().toString() !=
+    Object.getOwnPropertyNames(new Date()).sort().toString()
+  ) {
+    throw new Error("Invalid date!");
+  }
+
+  const month = date.getMonth() + 1;
+  if (month == 12 || month <= 2) {
+    return "winter";
+  } else if (3 <= month && month <= 5) {
+    return "spring";
+  } else if (6 <= month && month <= 8) {
+    return "summer";
+  } else if (9 <= month && month <= 11) {
+    return "autumn";
+  }
 }
 
 module.exports = {
